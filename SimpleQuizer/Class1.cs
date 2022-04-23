ec6e5409b989d34ce2b6e33cc1897b0b09ddeb4f
Василий <vasyasgors@gmail.com>
2022-04-23 18:00:37 +0300

    Initial commit

---
+++ SimpleQuizer/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpleQuizer
+{
+    public class Class1
+    {
+        public void Show()
+        {
+            System.Windows.Forms.MessageBox.Show("DLL");
+        }
+
+        public void Hide()
+        {
+
+        }
+
+        public void T()
+        {
+
+        }
+
+        public void R()
+        {
+            
+        }
+    }
+}
