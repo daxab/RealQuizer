2d8a43efa72ba5ae51596ed38b4900f7e8262202
Василий <vasyasgors@gmail.com>
2022-04-30 16:11:39 +0300

    f

--- QuizViewer/Form1.cs
+++ QuizViewer/Form1.cs
@@ -17,25 +17,5 @@ namespace SimpleQuizer.Viewer
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Quiz q = Quiz.GetTestQuiz();
-            ShowQuestion(q.Questions[0]);
-        }
-
-        private void ShowQuestion(Question question)
-        {
-            tableLayoutPanel1.Controls.Clear();
-            tableLayoutPanel1.RowStyles.Clear();
-
-            for (int i = 0; i < question.Answers.Count; i++)
-            {
-                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100 / question.Answers.Count));
-
-                tableLayoutPanel1.Controls.Add(new Label() { Text = question.Answers[i] }, 1, i);
-            }
-            
-        }
     }
 }
